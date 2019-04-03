58172e985408fb991e76481fe6475296d4cda28f
dev <bene.a99@gmail.com>
2019-04-03 10:12:00 +0200

    Initial Commit

---
+++ scrollView/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  scrollView
+//
+//  Created by Benjamin on 2019-04-03.
+//  Copyright © 2019 se.Benjamin.Aronsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //skapar frame
+        let rect2 = CGRect(x: 0, y: 0, width: view.frame.width, height: 90)
+        //skapar view
+        let bar = UIView(frame: rect2)
+        bar.backgroundColor = UIColor.blue
+        //add view
+        self.view.addSubview(bar)
+        
+        //skapar frame
+        let rect = CGRect(x: 100, y: 100, width: 100, height: 100)
+        //skapar view
+        let square = UIView(frame: rect)
+        square.backgroundColor = UIColor.purple
+        
+        let block = UIView(frame: rect)
+        block.backgroundColor = UIColor.blue
+        square.addSubview(block)
+        //add view
+        self.view.addSubview(square)
+        
+        
+        //skapa scrollview
+        let scrollView = UIScrollView(frame: view.bounds)
+        scrollView.backgroundColor = UIColor.green
+        
+        var offset : CGFloat = 0
+        
+        //skapar knapp
+        for i in 0...5 {
+            let place = CGRect(x: 100, y: 50 + i * 200, width: Int(view.frame.size.width - 200), height: 100)
+            let button = UIView(frame: place)
+            button.backgroundColor = UIColor.white
+            scrollView.addSubview(button)
+            offset = place.origin.y + place.size.height * 1.5
+        }
+        
+        //enable scroll
+        scrollView.contentSize = CGSize(width: view.bounds.size.width, height: offset)
+        
+        view.addSubview(scrollView)
+        
+        
+        
+        
+        
+        
+    }
+
+
+}
+
